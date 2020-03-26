04a471f0db956308a18cca58824398a9a7096009
Maria Clara <mc.holandalopes@gmail.com>
2020-03-26 18:26:55 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,13 @@
+module.exports = {
+    async indexedDB(request, response){
+        const ong_id = request.headers.authorization;
+
+        const incidents = await Connection('incidents')
+        .where('ong_id', ong_id)
+        .select('*');
+
+        return response.json(incidents);
+    
+    }
+
+}
